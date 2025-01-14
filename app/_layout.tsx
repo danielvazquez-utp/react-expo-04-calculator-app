0d4fc98f6790dd45ec1eec427f00e17775713e70
Paulo Daniel Vázquez Mora <daniel.vazquez@utpuebla.edu.mx>
2025-01-14 12:32:00 -0600

    Estilos y barra de estatus incorporados

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,24 @@
+import { Colors } from '@/constants/Colors';
+import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router';
-import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { View} from 'react-native'
 
 const RootLayout = () => {
+
+  const [ loaded ] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+  
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View>
-      <Text>RootLayout</Text>
+    <View style={{ backgroundColor: Colors.background, flex: 1 }}>
       {/* Slot busca el index.tsx de la carpeta */}
       <Slot />
-      <Text>Footer</Text>
+      <StatusBar style='light' />
     </View>
   )
 }
