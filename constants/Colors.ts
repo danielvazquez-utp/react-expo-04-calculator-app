0d228d0bf1a55be5138cde07a4673a9540c594a4
Paulo Daniel Vázquez Mora <daniel.vazquez@utpuebla.edu.mx>
2025-01-14 12:30:47 -0600

    Definición de colores como constantes

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -7,20 +7,10 @@ const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
-};
+  darkGray: '#2D2D2D',
+  lightGary: '#9B9B9B',
+  orange: '#FF9427',
+  textPrimary: 'white',
+  textSecondary: '#666',
+  background: '#000',
+} as const;
